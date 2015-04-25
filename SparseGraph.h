93d10a2fd1ae02e3b04caaf35c772dd9cd6b4afb
Taylor Foxhall <lxmachina@gmail.com>
2015-04-25 17:25:11 -0400

    sparse graph reps

---
+++ SparseGraph.h
@@ -0,0 +1,19 @@
+#ifndef __SPARSE_GRAPH_H
+#define __SPARSE_GRAPH_H
+
+#include <vector>
+
+#include "Vertex.h"
+
+template <class T>
+class SparseGraph
+{
+    public:
+        SparseGraph();
+        void insert(Vertex<T>);
+
+    private:
+        std::vector<Vertex<T>> verts;
+};
+
+#endif
