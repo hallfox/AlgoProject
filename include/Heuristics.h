783327ef60891e09b7452db239b3838fb4b1f6c7
Taylor Foxhall <lxmachina@gmail.com>
2015-05-01 12:46:01 -0400

    added pseudocode for a*

---
+++ include/Heuristics.h
@@ -0,0 +1,6 @@
+#ifndef __HEURISTICS_H
+#define __HEURISTICS_H
+
+int hDijkstra() { return 0; }
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 #include "SparseGraph.h"
 #include "Graph.h"
+#include "Heuristics.h"
 
 int dijkstra(Vertex start, Vertex end) {
   std::list<Edge> edges = start.getEdges();
@@ -9,6 +11,46 @@ int dijkstra(Vertex start, Vertex end) {
 }
 
 int aStar(Vertex start, Vertex end) {
+  std::vector<bool> visited(n, false); //nodes that have been visited
+  //this is used in calculating the estimated, showing how far we've already traveled
+  std::vector<int> fromDistance(n); 
+  //this is the estimated distance from start to end
+  std::vector<int> estDistance(n);
+  std::vector<int> backPath(n);
+
+  //the priority queue
+  //TODO: make the priority queue a min head based off of fromDistance
+  std::priority_queue<int> toVisit;
+  while(!toVisit.empty()) 
+  {
+    //pop and relax
+    int currVert = toVisit.pop();
+    if (currVert == end)
+    {
+        return path();
+    }
+
+    visited[currVert] = true;
+    std::vector<int> neighbors = graph.find(curr).getEdges();
+    for (std::vector<int>::const_iterator iter = neighbors.begin();
+            iter != neighbors.end(); iter++)
+    {
+        if (visited[*iter]) continue;
+        int tDist = fromDistance[currVert] + dist(currVert, *iter);
+
+        if (!toVisit.contains(*iter) || tDist < fromDistance[*iter])
+        {
+            backPath[*iter] = currVect;
+            fromDistance[*iter] = tDist;
+            estDistance[*iter] = tDist + hDist(*iter, end);
+            if (!toVisit.contains(*iter))
+            {
+                toVisit.push(*iter);
+            }
+        }
+    }
+  }
+
   return 0; 
 }
 
