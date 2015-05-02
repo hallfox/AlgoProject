dc375b85d598c3a07002ad22ed2bd0dd5f5811a1
Taylor Foxhall <lxmachina@gmail.com>
2015-05-02 18:35:40 -0400

    fixed up A* and added doc; finalized the pqueue and added note about lazy deletion; PLEASE READ

--- src/main.cpp
+++ src/main.cpp
@@ -20,31 +20,49 @@ void reheap(std::vector<VertexKey> heap)
 }
 
 int aStar(Vertex start, Vertex end) {
-  std::vector<bool> visited(n, false); // nodes that have been visited
-  // this is used in calculating the estimated, showing how far we've already traveled
+  // nodes that have been visited
+  std::vector<bool> visited(n, false);
+  // keeps track of shortest distance
   std::vector<int> fromDistance(n, std::numeric_limits<int>::max()); 
-  // this is the estimated distance from start to end
+  // keeps track of estimated shortest distance
   std::vector<int> estDistance(n);
+  // used to reconstruct where the shortest path came from
   std::vector<int> backPath(n);
 
   // the priority queue
   // A VertexKey is a pair<int, double> where int is the vertex id 
   // and double is the estimated distance
   std::priority_queue<VertexKey, minComp> toVisit;
+
+  // NOTE: since C++ doesn't support a decrease key op and we simply
+  // can't be bothered to write our own pqueue, we take the strategy
+  // of 'lazy deletion', meaning that when we update a vertex's estimated
+  // distance, we simply insert a new VertexKey with the updated distance
+  // and node id; because the new distance will always be smaller than the
+  // old key, we will always pop the better distance first, so we will
+  // always evaluate the nodes as if we called a decrease key
+  // This tradeoff requires that we use extra space, but it preserves time
+  // complexity O(logn) and we compensate by ignoring vertices that we've
+  // already processed
   while(!toVisit.empty()) 
   {
-    // pop and relax
+    // pop the best estimated distance off the pq
     VertexKey key = toVisit.top();
     int currVert = key.first;
     double currDist = key.second;
     toVisit.pop();
 
+    // check to make sure we haven't processed it already due to our
+    // 'lazy deletion' strategy
     if (visited[currVert]) continue;
+
+    // did we find the destination?
     if (currVert == end)
     {
         return path();
     }
 
+    // mark processed and iterate over the vertex's neighbors
     visited[currVert] = true;
     std::vector<Edge> neighbors = graph.find(currVert)->getEdges();
     for (std::vector<Edge>::const_iterator iter = neighbors.begin();
@@ -52,12 +70,17 @@ int aStar(Vertex start, Vertex end) {
     {
         int vNext = iter->end;
         double dist = iter->weight;
+
+        // get ready to relax the edge
         int tDist = fromDistance[currVert] + dist;
 
-        if (!visited[vNext] || tDist < fromDistance[*iter])
+        // check if we should relax
+        if (!visited[vNext] || tDist < fromDistance[vNext])
         {
             backPath[vNext] = currVect;
             fromDistance[vNext] = tDist;
+            // !IMPORTANT: this uses our approximation to head toward the destination
+            // h*() must never overestimate the actual distance, or the algorithm may be wrong 
             estDistance[vNext] = tDist + hDist(vNext, end);
             if (!visited[vNext])
             {
